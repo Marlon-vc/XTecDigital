64607080d9f423c43e30f696647dd683fa3c0d44
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-11-26 20:49:11 -0600

    Creado componente para inicializar el semestre

---
+++ XTecDigital/ClientApp/src/app/initialize-semester/initialize-semester.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initialize-semester',
+  templateUrl: './initialize-semester.component.html',
+  styleUrls: ['./initialize-semester.component.css']
+})
+export class InitializeSemesterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
