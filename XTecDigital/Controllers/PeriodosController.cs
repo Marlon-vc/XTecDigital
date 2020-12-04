9a67c535caf3f663bad77272521f02afacc7db94
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-12-03 20:50:49 -0600

    Creado el controlador de periodos

--- XTecDigital/Controllers/PeriodosController.cs
+++ XTecDigital/Controllers/PeriodosController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using XTecDigital.Models;
@@ -22,102 +18,28 @@ namespace XTecDigital.Controllers
 
         // GET: api/Periodos
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Periodo>>> GetPeriodo()
+        public IActionResult GetPeriodos()
         {
-            return await _context.Periodo.ToListAsync();
+            var result = _context.Periodo.FromSqlInterpolated($@"
+                EXECUTE dbo.sp_get_periods;
+            ");
+            return Ok(result);
         }
 
         // GET: api/Periodos/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Periodo>> GetPeriodo(int id)
+        public IActionResult GetPeriodo(int id)
         {
-            var periodo = await _context.Periodo.FindAsync(id);
+            var periodo = _context.Periodo.FromSqlInterpolated($@"
+                EXECUTE dbo.sp_get_period {id};
+            ").AsEnumerable().FirstOrDefault();
 
             if (periodo == null)
             {
                 return NotFound();
             }
 
-            return periodo;
-        }
-
-        // PUT: api/Periodos/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPeriodo(int id, Periodo periodo)
-        {
-            if (id != periodo.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(periodo).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PeriodoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Periodos
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Periodo>> PostPeriodo(Periodo periodo)
-        {
-            _context.Periodo.Add(periodo);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (PeriodoExists(periodo.Id))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetPeriodo", new { id = periodo.Id }, periodo);
-        }
-
-        // DELETE: api/Periodos/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Periodo>> DeletePeriodo(int id)
-        {
-            var periodo = await _context.Periodo.FindAsync(id);
-            if (periodo == null)
-            {
-                return NotFound();
-            }
-
-            _context.Periodo.Remove(periodo);
-            await _context.SaveChangesAsync();
-
-            return periodo;
-        }
-
-        private bool PeriodoExists(int id)
-        {
-            return _context.Periodo.Any(e => e.Id == id);
+            return Ok(periodo);
         }
     }
 }
