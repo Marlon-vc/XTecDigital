bd36b469b47c3f9fcd0da373161d23d2e4f24531
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-12-03 21:57:17 -0600

    Creados queries

--- DbScripts/SQLQuery1.sql
+++ DbScripts/SQLQuery1.sql
@@ -1,6 +1,13 @@
-INSERT INTO dbo.PERIODO (Id, Nombre)
-VALUES (1, '1'), (2, '2'), (3, 'V')
+INSERT INTO dbo.PERIODO (Nombre)
+VALUES ('1'), ('2'), ('V')
 
 SELECT * FROM dbo.PERIODO
 
-SELECT * FROM dbo.SEMESTRE;
+SELECT * 
+FROM dbo.SEMESTRE
+JOIN dbo.CURSO_SEMESTRE ON dbo.SEMESTRE.Id = dbo.CURSO_SEMESTRE.Id_semestre
+JOIN dbo.CURSO ON dbo.CURSO_SEMESTRE.Id_curso = dbo.CURSO.Codigo
+JOIN dbo.GRUPO ON dbo.GRUPO.Id_curso = dbo.CURSO.Codigo
+JOIN dbo.RUBRO ON dbo.RUBRO.Id_grupo = dbo.GRUPO.Id
+JOIN dbo.GRUPO_ESTUDIANTE ON dbo.GRUPO_ESTUDIANTE.Numero_Grupo = dbo.GRUPO.Id
+JOIN dbo.GRUPO_PROFESOR ON dbo.GRUPO_PROFESOR.Numero_Grupo = dbo.GRUPO.Id;
