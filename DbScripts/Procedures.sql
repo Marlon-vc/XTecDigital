c51d37666049bb7b9dc6edea5a3aafb1375261bd
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-12-03 20:49:01 -0600

    Creados los procedimientos para inicializar un semestre

--- DbScripts/Procedures.sql
+++ DbScripts/Procedures.sql
@@ -1,5 +1,5 @@
 -- Procedimientos almacenados
-
+-- Procedimientos almacenados de cursos
 GO
 CREATE PROCEDURE dbo.sp_get_courses 
 AS
@@ -25,7 +25,7 @@ SELECT Codigo, Nombre, Carrera, Habilitado
 FROM dbo.CURSO
 WHERE Codigo = @CourseId;
 
-EXECUTE dbo.sp_get_course 'CE1101';
+-- EXECUTE dbo.sp_get_course 'CE1101';
 
 GO
 CREATE PROCEDURE dbo.sp_set_course_state
@@ -71,3 +71,85 @@ SET Nombre = @Nombre, Carrera = @Carrera, Habilitado = @Habilitado
 WHERE Codigo = @Codigo;
 
 -- EXECUTE dbo.sp_update_course 'CE1101', 'Introducción a la programación', 'Computadores', 1;
+
+-- Procedimientos almacenados de periodo
+GO 
+CREATE PROCEDURE dbo.sp_get_periods
+AS
+SELECT Id, Nombre 
+FROM dbo.PERIODO;
+
+GO 
+CREATE PROCEDURE dbo.sp_get_period
+	@periodId INT
+AS
+SELECT Id, Nombre 
+FROM dbo.PERIODO
+WHERE Id = @periodId;
+
+-- Procedimientos para inicializar semestre
+GO 
+CREATE PROCEDURE dbo.sp_create_semester
+	@semesterId INT,
+	@anioSemester INT,
+	@idPeriodo INT
+AS
+INSERT INTO dbo.SEMESTRE (Id, Anio, Id_periodo)
+VALUES (@semesterId, @anioSemester, @idPeriodo);
+
+GO 
+CREATE PROCEDURE dbo.sp_create_curso_semestre
+	@idCursoSemestre INT,
+	@idCurso VARCHAR(50),
+	@idSemestre INT
+AS
+INSERT INTO dbo.CURSO_SEMESTRE (Id, Id_curso, Id_semestre)
+VALUES (@idCursoSemestre, @idCurso, @idSemestre);
+
+GO
+CREATE PROCEDURE dbo.sp_create_grupo
+	@numeroGrupo INT,
+	@idCurso VARCHAR(50)
+AS
+INSERT INTO dbo.GRUPO (Numero, Id_curso)
+VALUES (@numeroGrupo, @idCurso);
+
+GO
+CREATE PROCEDURE dbo.sp_create_grupo_estudiante
+	@numeroGrupo INT,
+	@carnet VARCHAR(50)
+AS
+INSERT INTO dbo.GRUPO_ESTUDIANTES (Numero_Grupo, Estudiante)
+VALUES (@numeroGrupo, @carnet);
+
+GO
+CREATE PROCEDURE dbo.sp_create_grupo_profesor
+	@numeroGrupo INT,
+	@cedula VARCHAR(50)
+AS
+INSERT INTO dbo.GRUPO_PROFESOR (Numero_Grupo, Profesor)
+VALUES (@numeroGrupo, @cedula);
+
+GO
+CREATE PROCEDURE dbo.sp_create_carpeta
+	@idCarpeta INT,
+	@soloLectura BIT,
+	@ruta VARCHAR(50),
+	@idGrupo INT,
+	@nombreCarpeta VARCHAR(50)
+AS
+INSERT INTO dbo.CARPETA (Id, Nombre, Solo_lectura, Ruta, Id_grupo)
+VALUES (@idCarpeta, @nombreCarpeta, @soloLectura, @ruta, @idGrupo);
+
+GO
+CREATE PROCEDURE dbo.sp_create_rubro
+	@nombreRubro VARCHAR(50),
+	@porcentaje DECIMAL(5,2),
+	@idGrupo INT
+AS
+INSERT INTO dbo.RUBRO (Nombre, Porcentaje, Id_grupo)
+VALUES (@nombreRubro, @porcentaje, @idGrupo);
+
+
+
+
