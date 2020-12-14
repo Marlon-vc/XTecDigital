ebec12d4e57466c6df165859540dca7c0579cad9
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-12-14 16:47:03 -0600

    Creado panel lateral

--- XTecDigital/ClientApp/src/app/sidebar/sidebar.component.ts
+++ XTecDigital/ClientApp/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Grupo } from '../models/grupo';
+import { Semestre } from '../models/semestre';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,39 +9,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  userType = window.localStorage.getItem('user-type');
+  user = window.localStorage.getItem('user-id');
+  semestres: Semestre[] = [];
+  actualUser;
+
   constructor() { }
 
   ngOnInit(): void {
 
-    $("#close-sidebar").click(function() {
+    // this.userType = 'profesor';
+    // this.userType = 'admin';
+    // this.userType = 'estudiante';
+
+    $("#close-sidebar").on('click', function() {
       $(".page-wrapper").removeClass("toggled");
     });
-    $("#show-sidebar").click(function() {
+    $("#show-sidebar").on('click', function() {
       $(".page-wrapper").addClass("toggled");
     });
+    console.log("settings handlers");
 
-    $(".sidebar-dropdown > a").click(function() {
-      $(".sidebar-submenu").slideUp(200);
-      if (
-        $(this)
-          .parent()
-          .hasClass("active")
-      ) {
-        $(".sidebar-dropdown").removeClass("active");
-        $(this)
-          .parent()
-          .removeClass("active");
-      } else {
-        $(".sidebar-dropdown").removeClass("active");
-        $(this)
-          .next(".sidebar-submenu")
-          .slideDown(200);
-        $(this)
-          .parent()
-          .addClass("active");
-      }
+    $(function() {
+      $('li.sidebar-dropdown>a').on('click', function() {
+        console.log('clicked');
+        $(".sidebar-submenu").slideUp(200);
+        if (
+          $(this)
+            .parent()
+            .hasClass("active")
+        ) {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this)
+            .parent()
+            .removeClass("active");
+        } else {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this)
+            .next(".sidebar-submenu")
+            .slideDown(200);
+          $(this)
+            .parent()
+            .addClass("active");
+        }
+        return false;
+      });
     });
+
+    if (this.userType == 'estudiante') {
+      this.loadSemestres();
+    }
+    this.getActualUserInfo();
+
+  }
+
+  loadSemestres() {
+    // cargar semestres del estudiante actual
+    var grupo1 = new Grupo();
+    var grupo2 = new Grupo();
+    var grupo3 = new Grupo();
+    var grupo4 = new Grupo();
+
+    grupo1.idCurso = 'CE1';
+    grupo1.numero = 1;
+    grupo2.idCurso = 'CE2';
+    grupo2.numero = 2;
+    grupo3.idCurso = 'CE3';
+    grupo3.numero = 3;
+    grupo4.idCurso = 'CE4';
+    grupo4.numero = 4;
+
+    var sem = new Semestre();
+    sem.anio = 2020;
+    sem.id = 1;
+    sem.periodo = '1';
+    sem.grupos = [grupo1, grupo2, grupo3, grupo4];
     
+    this.semestres.push(sem);
+  }
+
+  getActualUserInfo() {
+    var user = {
+      nombre: 'Paola Villegas',
+      tipo: 'Estudiante'
+    }
+
+    this.actualUser = user;
+  }
+
+  onLogout() {
+    console.log('logging out..');
+    window.localStorage.clear();
   }
 
 
