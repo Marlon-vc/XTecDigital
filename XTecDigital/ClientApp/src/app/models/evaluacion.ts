7648096c95f421764cd6470595469923334ebd56
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-12-17 14:47:05 -0600

    Agregado modelo de evaluacion

---
+++ XTecDigital/ClientApp/src/app/models/evaluacion.ts
@@ -0,0 +1,13 @@
+export class Evaluacion {
+
+    id: number;
+    idRubro: number;
+    idEspecificacion: number;
+    nombre: string;
+    notasPublicadas: boolean;
+    pesoNota: number;
+    grupal: boolean;
+    fechaEntrega: string;
+
+    constructor() { }
+}
