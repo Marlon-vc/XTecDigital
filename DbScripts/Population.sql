1a0d8c51be7ebc30b70d55e90d61d7168424e824
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-12-18 15:18:32 -0600

    Update Population.sql

--- DbScripts/Population.sql
+++ DbScripts/Population.sql
@@ -22,3 +22,15 @@ JOIN dbo.CARPETA ON dbo.GRUPO.Id = dbo.CARPETA.Id_grupo
 
 select * from dbo.RUBRO
 WHERE Id_grupo = 1
+
+select * from dbo.NOTICIA
+select * from dbo.GRUPO
+
+
+SELECT dbo.SEMESTRE.Id AS Id_semestre, Anio, Periodo, dbo.GRUPO.Id AS Id_grupo, Numero as Numero_grupo, Id_curso, 
+	Nombre, Creditos, Carrera, Profesor, Estudiante
+FROM dbo.SEMESTRE 
+JOIN dbo.GRUPO ON Id_semestre = dbo.SEMESTRE.Id
+JOIN dbo.CURSO ON Id_curso = dbo.CURSO.Codigo
+JOIN dbo.PROFESOR_GRUPO ON dbo.PROFESOR_GRUPO.Id_grupo = dbo.GRUPO.Id 
+JOIN dbo.ESTUDIANTE_GRUPO ON dbo.ESTUDIANTE_GRUPO.Id_grupo = dbo.GRUPO.Id;
