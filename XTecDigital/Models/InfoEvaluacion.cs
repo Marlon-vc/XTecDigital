38bda9ee28c6169dfae55f14acc078c306405dc0
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2021-01-13 18:12:57 -0600

    Creados modelos de informacion de evaluacion, vista

---
+++ XTecDigital/Models/InfoEvaluacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XTecDigital.Models
+{
+    public partial class InfoEvaluacion
+    {
+        public int IdEvaluacion { get; set; }
+        public string ArchivoEspecificacion { get; set; }
+        public DateTime? FechaCreacionEspec { get; set; }
+        public int? IdEspec { get; set; }
+        public string ArchivoEntregable { get; set; }
+        public DateTime? FechaCreacionEntre { get; set; }
+        public int? IdEntre { get; set; }
+        public int? IdGrupo { get; set; }
+        public decimal? NotaFinal { get; set; }
+        public string Observaciones { get; set; }
+        public string Retroalimentacion { get; set; }
+        public int? IdRetro { get; set; }
+    }
+}
