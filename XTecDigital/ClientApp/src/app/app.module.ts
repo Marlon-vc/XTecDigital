e65c192dc1aabbf8dadb672f6188860a63cab763
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2021-01-18 13:53:07 -0600

    Eliminados componentes innecesarios

--- XTecDigital/ClientApp/src/app/app.module.ts
+++ XTecDigital/ClientApp/src/app/app.module.ts
@@ -5,14 +5,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
 import { InitializeSemesterComponent } from './initialize-semester/initialize-semester.component'
 import { LoginComponent } from './login/login.component';
 import { GestionCursosComponent } from './gestion-cursos/gestion-cursos.component';
 import { GestionDocumentosComponent } from './gestion-documentos/gestion-documentos.component';
 import { GestionRubrosComponent } from './gestion-rubros/gestion-rubros.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeGrupoComponent } from './home-grupo/home-grupo.component';
 import { NewsManagementComponent } from './news-management/news-management.component';
 import { SeeGroupNewsComponent } from './see-group-news/see-group-news.component';
@@ -27,14 +24,11 @@ import { GroupInfoComponent } from './group-info/group-info.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
     InitializeSemesterComponent,
     LoginComponent,
     GestionCursosComponent,
     GestionDocumentosComponent,
     GestionRubrosComponent,
-    SidebarComponent,
     HomeGrupoComponent,
     NewsManagementComponent,
     SeeGroupNewsComponent,
@@ -52,12 +46,10 @@ import { GroupInfoComponent } from './group-info/group-info.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: LoginComponent, pathMatch: 'full' },
-      { path: 'home', component: HomeComponent },
       { path: 'initialize-semester', component: InitializeSemesterComponent},
       { path: 'gestion-cursos', component: GestionCursosComponent },
       { path: 'gestion-documentos', component: GestionDocumentosComponent },
       { path: 'gestion-rubros', component: GestionRubrosComponent},
-      { path: 'sidebar', component: SidebarComponent},
       { path: 'documentos', component: GestionDocumentosComponent },
       { path: 'grupo', component: GroupInfoComponent },
       { path: 'noticias', component: NewsManagementComponent },
