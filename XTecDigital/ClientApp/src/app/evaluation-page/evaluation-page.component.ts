c66c30cc97b4cb5e4c100881a786d9f79ed1c0c7
Marlon Vega C <mvegach2013@gmail.com>
2021-01-18 21:27:29 -0600

    Arreglados los requests al api

--- XTecDigital/ClientApp/src/app/evaluation-page/evaluation-page.component.ts
+++ XTecDigital/ClientApp/src/app/evaluation-page/evaluation-page.component.ts
@@ -15,49 +15,63 @@ export class EvaluationPageComponent implements OnInit {
 
   rubros: Rubro[] = [];
   change = false;
-  groupId = this.route.snapshot.params.id;
+  group: any;
 
   constructor(private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit(): void {
+    let groupInfo = JSON.parse(window.localStorage.getItem('group'));
+    console.log(groupInfo);
+
+    if (groupInfo == undefined) {
+      console.log('No group selected');
+      return;
+    }
+    
+    this.group = {
+      Numero: groupInfo.numeroGrupo,
+      Curso: groupInfo.codigo,
+      Anio: groupInfo.anioSemestre,
+      Periodo: groupInfo.periodoSemestre
+    };
     this.loadRubros();
   }
 
   /**
    * Metodo para cargar los rubros
    */
-  getRubros() {
-    this.api.get(`https://localhost/api/Rubros/Grupo/${this.groupId}`).subscribe(
-      (rubros: Rubro[]) => {
-        // console.log(rubros);
-        rubros.forEach(rubro => {
-          // console.log(rubro);
-          this.api.get(`https://localhost/api/Evaluaciones/Rubro/`).subscribe(
-            (evaluaciones:Evaluacion[]) => {
-              // console.log(evaluaciones);
-              rubro.evaluaciones = evaluaciones;
-              rubro.evaluaciones.forEach(evaluacion => {
-                this.api.get(`https://localhost/api/Evaluaciones/Info/${evaluacion.id}`).subscribe(
-                  (info:InfoEvaluacion) => {
-                    console.log(info);
-                    evaluacion.info = info;
-                  }, (error:any) => {
-                    console.log(error);
-                  }
-                );
-              });
-              this.rubros = rubros;
-              console.log(this.rubros);
-            }, (error: any) => {
-              console.log(error);
-            }
-          );
-        });
-      }, (error: any) => {
-        console.log(error);
-      }
-    );
-  }
+  // getRubros() {
+  //   this.api.get(`https://localhost/api/Rubros/Grupo/${this.groupId}`).subscribe(
+  //     (rubros: Rubro[]) => {
+  //       // console.log(rubros);
+  //       rubros.forEach(rubro => {
+  //         // console.log(rubro);
+  //         this.api.get(`https://localhost/api/Evaluaciones/Rubro/`).subscribe(
+  //           (evaluaciones:Evaluacion[]) => {
+  //             // console.log(evaluaciones);
+  //             rubro.evaluaciones = evaluaciones;
+  //             rubro.evaluaciones.forEach(evaluacion => {
+  //               this.api.get(`https://localhost/api/Evaluaciones/Info/${evaluacion.id}`).subscribe(
+  //                 (info:InfoEvaluacion) => {
+  //                   console.log(info);
+  //                   evaluacion.info = info;
+  //                 }, (error:any) => {
+  //                   console.log(error);
+  //                 }
+  //               );
+  //             });
+  //             this.rubros = rubros;
+  //             console.log(this.rubros);
+  //           }, (error: any) => {
+  //             console.log(error);
+  //           }
+  //         );
+  //       });
+  //     }, (error: any) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
 
   /**
    * Metodo asincrono para cargar rubros
@@ -65,7 +79,6 @@ export class EvaluationPageComponent implements OnInit {
   async loadRubros() {
     this.rubros = await this.getRubros2();
     console.log(this.rubros);
-    
   }
 
   /**
@@ -73,7 +86,10 @@ export class EvaluationPageComponent implements OnInit {
    */
   getRubros2(): Promise<Rubro[]> {
     return new Promise((resolve, reject) => {
-      this.api.get(`https://localhost/api/Rubros/Grupo/${this.groupId}`).subscribe(
+
+      let query = new URLSearchParams(this.group).toString();
+
+      this.api.get(`https://localhost/api/Rubros/Grupo?${query}`).subscribe(
         async (rubros: Rubro[]) => {
           for (let i = 0; i < rubros.length; i++) {
             const current = rubros[i];
@@ -94,15 +110,24 @@ export class EvaluationPageComponent implements OnInit {
    */
   async getEvaluacionesRubro(rubro: Rubro): Promise<Evaluacion[]> {
     return new Promise((resolve, reject) => {
-      this.api.get(`https://localhost/api/Evaluaciones/Rubro/`).subscribe(
-        async (evaluaciones: Evaluacion[]) => {
+      let data: any = {
+        Nombre: rubro.nombre,
+        Numero: rubro.numero,
+        Curso: rubro.curso,
+        Anio: rubro.anio,
+        Periodo: rubro.periodo
+      }
+
+      let query = new URLSearchParams(data).toString();
+      this.api.get(`https://localhost/api/Evaluaciones/Rubro?${query}`).subscribe(
+        async (evaluaciones: any[]) => {
           for (let i = 0; i < evaluaciones.length; i++) {
             const evaluacion = evaluaciones[i];
             evaluacion.info = await this.getInfoEvaluacion(evaluacion);
           }
           resolve(evaluaciones);
         }, (error) => {
-          console.log(error);
+          // console.log(error);
           reject(error);
         }
       );
@@ -113,9 +138,18 @@ export class EvaluationPageComponent implements OnInit {
    * Metodo asincrono para obtener la informacion de una evaluacion
    * @param evaluacion  objeto tipo evaluacion
    */
-  async getInfoEvaluacion(evaluacion: Evaluacion): Promise<InfoEvaluacion> {
+  async getInfoEvaluacion(evaluacion: any): Promise<InfoEvaluacion> {
     return new Promise((resolve, reject) => {
-      this.api.get(`https://localhost/api/Evaluaciones/Info/${evaluacion.id}`).subscribe(
+
+      // let data = {
+      //   Nombre: evaluacion.nombre,
+      //   Rubro: evaluacion.ru
+      // }
+
+      let query = new URLSearchParams(evaluacion).toString();
+      console.log(query);
+      
+      this.api.get(`https://localhost/api/Evaluaciones/Info?${query}`).subscribe(
         (info: InfoEvaluacion) => {
           resolve(info);
         }, (error) => {
